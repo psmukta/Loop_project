404ed5ec2f6b63618cdb3690b10c1822612f68d3
psmukta <psmukta@yahoo.com>
2021-02-04 21:30:41 -0500

    file add

--- LoopProject/src/forLoopPack/WhileLoopClass.java
+++ LoopProject/src/forLoopPack/WhileLoopClass.java
@@ -10,7 +10,22 @@ public class WhileLoopClass {
 			x++;
 		}
 		System.out.println(sum);
-	}
-	
 
+
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+		}
+
+		for(int i=5;i>=1;i--) {
+			System.out.println(i);
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}	
+		}
+	}
 }
+
+
--- LoopProject/src/switchPack/SwitchBreakClass.java
+++ LoopProject/src/switchPack/SwitchBreakClass.java
@@ -1,4 +1,4 @@
-package switchPack;
+ package switchPack;
 
 public class SwitchBreakClass {
 
